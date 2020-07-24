30b7d6aad3e0694da9a7bc6a461b7bf05801849a
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-07-24 15:14:21 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+/* main.js */
+
+var canvas = document.getElementById("myChart");
+var context = canvas.getContext("2d");
+var myChart = new Chart(context, {
+	type: "bar",
+	data: {
+		labels: ["Red", "Blue", "Yellow"],
+		datasets: [{
+			label: "# od Votes",
+			data: [12, 19, 3],
+			backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)'
+			],
+			borderColor: [
+				'rgba(255, 99, 132, 1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 206, 86, 1)'
+			],
+			borderWidth: 1
+		}]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		}
+	}
+})
